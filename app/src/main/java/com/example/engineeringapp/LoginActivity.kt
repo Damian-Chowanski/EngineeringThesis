a0d9c81ded8c8b642d7961a41006a6ba447c84f2
Damian-Chowanski <dchowanski30@gmail.com>
2022-12-31 15:30:56 +0100

    registration screen created and added activity between screens

--- app/src/main/java/com/example/engineeringapp/LoginActivity.kt
+++ app/src/main/java/com/example/engineeringapp/LoginActivity.kt
@@ -1,13 +1,21 @@
 package com.example.engineeringapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val registration = findViewById<TextView>(R.id.registration_activity_text)
+        registration.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
--- app/src/main/java/com/example/engineeringapp/RegistrationActivity.kt
+++ app/src/main/java/com/example/engineeringapp/RegistrationActivity.kt
@@ -1,11 +1,21 @@
 package com.example.engineeringapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegistrationActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        val logInActivity = findViewById<TextView>(R.id.login_activity_text)
+        logInActivity.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
