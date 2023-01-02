6166aa2ba8414fac32875142f0dd18d635af8d76
Damian-Chowanski <dchowanski30@gmail.com>
2023-01-02 16:48:00 +0100

    Registration's Edit text condition added

--- app/src/main/java/com/example/engineeringapp/RegistrationActivity.kt
+++ app/src/main/java/com/example/engineeringapp/RegistrationActivity.kt
@@ -1,10 +1,13 @@
 package com.example.engineeringapp
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class RegistrationActivity : AppCompatActivity() {
 
@@ -13,6 +16,42 @@ class RegistrationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_registration)
         this.title = "Registration Page"
 
+        val btnRegister = findViewById<Button>(R.id.btn_register)
+        var reg_email = findViewById<EditText>(R.id.register_email)
+        var reg_pass1 = findViewById<EditText>(R.id.registration_password)
+        var reg_pass2 = findViewById<EditText>(R.id.repeat_password)
+        btnRegister.setOnClickListener {
+            when{
+                TextUtils.isEmpty(reg_email.text.toString().trim { it <= ' '}) -> {
+                    Toast.makeText(
+                        this@RegistrationActivity,
+                        "Please enter an email",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                TextUtils.isEmpty(reg_pass1.text.toString().trim { it <= ' '}) -> {
+                    Toast.makeText(
+                        this@RegistrationActivity,
+                        "Please enter a password",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                TextUtils.isEmpty(reg_pass2.text.toString().trim {it <= ' '}) -> {
+                    Toast.makeText(
+                        this@RegistrationActivity,
+                        "Please repeat your password",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                !TextUtils.equals(reg_pass1.text.toString(),reg_pass2.text.toString()) -> {
+                    Toast.makeText(
+                        this@RegistrationActivity,
+                        "The passwords don't match!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
         //Go to Login Activity:
         val logInActivity = findViewById<TextView>(R.id.login_activity_text)
         logInActivity.setOnClickListener {
