94a020c6a8ae87354db32008b2b6a982c3cde68d
Damian-Chowanski <dchowanski30@gmail.com>
2023-01-12 19:01:06 +0100

    logout functionality added

--- app/src/main/java/com/example/engineeringapp/MainActivity.kt
+++ app/src/main/java/com/example/engineeringapp/MainActivity.kt
@@ -5,14 +5,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.engineeringapp.UI_login.LoginActivity
 import com.example.engineeringapp.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    lateinit var topAppBar: MaterialToolbar
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        topAppBar = findViewById(R.id.topAppBar)
         //val userId = intent.getStringExtra("user_id")
         val userId = FirebaseAuth.getInstance().uid
         val emailId = FirebaseAuth.getInstance().currentUser!!.email
@@ -30,12 +33,25 @@ class MainActivity : AppCompatActivity() {
 
         val btnLogout: Button = findViewById(R.id.btn_logout)
         btnLogout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
+            logout()
+        }
 
-            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
-            finish()
+        topAppBar.setOnMenuItemClickListener() { menuItem ->
+            when (menuItem.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.ab_logout -> {
+                    logout()
+                    true
+                }
+                else -> { false }
+            }
         }
     }
+
+
     //hide action bar
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
@@ -43,4 +59,12 @@ class MainActivity : AppCompatActivity() {
             this.window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN)
         }
     }
+
+    fun logout() {
+        FirebaseAuth.getInstance().signOut()
+
+        startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        finish()
+    }
 }
+
